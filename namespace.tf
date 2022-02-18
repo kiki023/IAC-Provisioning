591287e092adcac08957d9cc45417cb477a9fccd
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 10:22:00 +0100

    Update namespace.tf

--- namespace.tf
+++ namespace.tf
@@ -1,7 +1,7 @@
 variable "namespaces" {
   type        = list(string)
   description = "List of namespaces to be created in our EKS Cluster."
-  default     = "dotpaystaging"
+  name     = "dotpaystaging"
 }
 
 resource "kubernetes_namespace" "eks_namespaces" {
