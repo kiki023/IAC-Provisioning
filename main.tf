7035b8ef510b4d340962d89da47be3f11a8dd1de
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 09:50:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ terraform {
   required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
 
   required_providers {
+    
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.20.0"
+    } 
+    
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.7.0"
-    }
-  }
+      
+    } 
 
     random = {
       source  = "hashicorp/random"
