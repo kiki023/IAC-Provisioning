7b77972a5445bca9eb1e7e599e28d897e950ab8f
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 13:42:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 terraform {
   
+  backend "s3" {
+    
+      bucket         = "dotpaystaging"
+      key            = "s3/terraform.tfstate"
+      region         = "us-west-1"
+
+      dynamodb_table = "dotpaydB"
+    
+   }
+
+  
   required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
 
   required_providers {
@@ -35,4 +46,17 @@ terraform {
       version = ">=2.0.1"
     }
   }
+}
+
+   
+ resource "aws_dynamodb_table" "terraform_statelock" {
+    name = "${var.aws_dynamodb_table}"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+   }
 }
