c3399af3bda8865816a3f994b6a6a72cc5aba276
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 09:29:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
   required_version = ">= 0.13"
 
-required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=3.20.0"
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
+  }
 
     random = {
       source  = "hashicorp/random"
