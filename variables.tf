243ddb46be977e04e2522faf454b64df52e45e7d
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 09:42:37 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,15 +14,10 @@ variable "eks_node_instance_type" {
   default = "t2.medium"
 }
 
-# backend
-bucket               = "dotpaystaging"
-region               = "us-west-1"
-key                  = "terraform.tfstate"
-dynamodb_table       = "dotpaydb"
 
 # deployment sub_domain
 # deployments_subdomains = ["sample"]
 
 # namespace
-namespaces = ["dotpay-staging"]
+namespaces = "dotpay-staging"
 
