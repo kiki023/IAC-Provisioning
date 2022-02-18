a60b13b8dd41a458a0e6853ff20eb1f0262b60d7
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 09:00:25 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "dotpay-staging" {
   cidr_block = "10.0.0.0/16"
 
   tags = tomap({
-    "Name" = "dotpay-staging-node",
+    "Name" = "terraform-eks-demo-node",
     "kubernetes.io/cluster/${var.cluster-name}" = "shared",
   })
 }
