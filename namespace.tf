8acb261b03ed84f4cb3ebe71b6f4c1a4e7755186
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 09:11:46 +0100

    Create namespace.tf

---
+++ namespace.tf
@@ -0,0 +1,16 @@
+variable "namespaces" {
+  type        = list(string)
+  description = "List of namespaces to be created in our EKS Cluster."
+}
+
+# create all Namespaces into EKS
+resource "kubernetes_namespace" "eks_namespaces" {
+  for_each = toset(var.namespaces)
+
+  metadata {
+    annotations = {
+      name = each.key
+    }
+    name = each.key
+  }
+}
