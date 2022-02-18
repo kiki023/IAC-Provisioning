b84da79ec81bb1ad35c8a835425d81f7465fa1db
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 11:37:24 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,42 +6,39 @@
 #  * Route Table
 #
 
-resource "aws_vpc" "dotpay-staging" {
+resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
 
-  tags = tomap({
-    "Name" = "terraform-eks-demo-node",
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
-  })
-}
-data "aws_availability_zones" "available" {
-  state = "available"
+  tags = map(
+    "Name", "terraform-eks-demo-node",
+    "kubernetes.io/cluster/${var.cluster-name}", "shared",
+  )
 }
 
-resource "aws_subnet" "dotpay" {
+resource "aws_subnet" "demo" {
   count = 2
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   cidr_block              = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = true
-  vpc_id                  = aws_vpc.dotpay-staging.id
+  vpc_id                  = aws_vpc.demo.id
 
-  tags = tomap({
-    "Name" = "dotpay-staging-node",
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
-  })
+  tags = map(
+    "Name", "terraform-eks-demo-node",
+    "kubernetes.io/cluster/${var.cluster-name}", "shared",
+  )
 }
 
-resource "aws_internet_gateway" "dp" {
-  vpc_id = aws_vpc.dotpay-staging.id
+resource "aws_internet_gateway" "demo" {
+  vpc_id = aws_vpc.demo.id
 
   tags = {
-    Name = "dotpay-staging"
+    Name = "terraform-eks-demo"
   }
 }
 
 resource "aws_route_table" "demo" {
-  vpc_id = aws_vpc.dotpay-staging.id
+  vpc_id = aws_vpc.demo.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -52,6 +49,6 @@ resource "aws_route_table" "demo" {
 resource "aws_route_table_association" "demo" {
   count = 2
 
-  subnet_id      = aws_subnet.dotpay.*.id[count.index]
+  subnet_id      = aws_subnet.demo.*.id[count.index]
   route_table_id = aws_route_table.demo.id
 }
