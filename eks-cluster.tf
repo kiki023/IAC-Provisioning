6f550af9246e3be2ca20477d20ceb264871b6477
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 11:29:13 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,11 +1,5 @@
-# EKS Cluster Resources
-#  * IAM Role to allow EKS service to manage other AWS services
-#  * EC2 Security Group to allow networking traffic with EKS cluster
-#  * EKS Cluster
-#
-
 resource "aws_iam_role" "demo-cluster" {
-  name = "dotpay-staging-cluster"
+  name = "terraform-eks-demo-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -34,10 +28,10 @@ resource "aws_iam_role_policy_attachment" "demo-cluster-AmazonEKSServicePolicy" 
 }
 
 resource "aws_security_group" "demo-cluster" {
-  name        = "dotpay-staging-cluster"
+  name        = "terraform-eks-demo-cluster"
   description = "Cluster communication with worker nodes"
-  vpc_id      = aws_vpc.dotpay-staging.id
-  
+  vpc_id      = aws_vpc.demo.id
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -45,20 +39,19 @@ resource "aws_security_group" "demo-cluster" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-tags = {
-    Name = "dotpay-staging-demo"
+  tags = {
+    Name = "terraform-eks-demo"
   }
 }
 
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   cidr_blocks       = [local.workstation-external-cidr]
   description       = "Allow workstation to communicate with the cluster API Server"
-  from_port         = 0
+  from_port         = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.demo-cluster.id
-  to_port           = 443 
+  to_port           = 443
   type              = "ingress"
-
 }
 
 resource "aws_eks_cluster" "demo" {
@@ -67,7 +60,7 @@ resource "aws_eks_cluster" "demo" {
 
   vpc_config {
     security_group_ids = [aws_security_group.demo-cluster.id]
-    subnet_ids         = aws_subnet.dotpay[*].id
+    subnet_ids         = aws_subnet.demo[*].id
   }
 
   depends_on = [
