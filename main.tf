ff201c7044d2dc4895bdc92b061f6d1f1e91fbe6
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 08:17:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,33 @@
 terraform {
   required_version = ">= 0.13"
 
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.20.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.0.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = ">=2.0.1"
     }
   }
 
- provider "aws" {
-        region = "us-west-1"
-        shared_credentials_file = "~/eksadmin/.aws/credentials"
-}
  backend "s3" {}
 
    required_version = "~> 0.14"
