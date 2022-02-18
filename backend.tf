6d1be9f82d35b1c09823ffc17e3ad76eccb2a42f
Lovelyn Chinenye <88979683+kiki023@users.noreply.github.com>
2022-02-18 13:30:50 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,4 @@
-terraform }
+terraform {
   backend "s3" {
     
       bucket         = "dotpaystaging"
@@ -21,6 +21,6 @@ terraform }
   attribute {
       name = "LockID"
       type = "S"
+    }
   }
-
 }
